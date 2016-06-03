c73e016829e2f582e818189f0bca49c0b5147184
Luud Janssen <luud@bitsensor.io>
2016-06-03 21:10:19 +0200

    Add about templates & set fallback theme

--- templates/templates.php
+++ templates/templates.php
@@ -7,7 +7,8 @@ class TemplateController {
     private $templates;
 
     public function __construct($templates) {
-        $this->theme = 'default';
+        $this->theme = 'space';
+        $this->fallBackTheme = 'default';
         $this->templates = new League\Plates\Engine($templates, 'php');
 
         $this->templates->registerFunction('externalLink', function ($url, $text = null) {
@@ -22,7 +23,13 @@ class TemplateController {
     
     // Render a template directly
     public function render($index, $variables) {
-        return $this->templates->render($this->theme.'/templates/'.$index, $variables);
+
+        // If the template doesn't exist fall back to the fallback theme
+        if ($this->templates->exists($this->theme.'/templates/'.$index)) {
+            return $this->templates->render($this->theme.'/templates/'.$index, $variables);
+        } else {
+            return $this->templates->render($this->fallBackTheme.'/templates/'.$index, $variables);
+        }
     }
 
     public function getTemplateVariables($path = '') {
