9675518a7c540711912d81ca71f00f255754fbd7
Luud Janssen <luud@bitsensor.io>
2016-06-03 17:15:21 +0200

    Cleanup captcha template

--- vulnerabilities/captcha/index.php
+++ vulnerabilities/captcha/index.php
@@ -3,22 +3,22 @@
 $dir = dirname(__FILE__);
 $templates = require $dir.'/../../templates/templates.php';
 
-define( 'DVWA_WEB_PAGE_TO_ROOT', '../../' );
+define('DVWA_WEB_PAGE_TO_ROOT', '../../');
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 require_once DVWA_WEB_PAGE_TO_ROOT . "external/recaptcha/recaptchalib.php";
 
-dvwaPageStartup( array( 'authenticated', 'phpids' ) );
+dvwaPageStartup(array('authenticated', 'phpids'));
 
 $page = dvwaPageNewGrab();
-$page[ 'title' ]   = 'Vulnerability: Insecure CAPTCHA' . $page[ 'title_separator' ].$page[ 'title' ];
-$page[ 'page_id' ] = 'captcha';
-$page[ 'help_button' ]   = 'captcha';
-$page[ 'source_button' ] = 'captcha';
+$page['title']   = 'Vulnerability: Insecure CAPTCHA' . $page['title_separator'].$page['title'];
+$page['page_id'] = 'captcha';
+$page['help_button']   = 'captcha';
+$page['source_button'] = 'captcha';
 
 dvwaDatabaseConnect();
 
 $vulnerabilityFile = '';
-switch( $_COOKIE[ 'security' ] ) {
+switch ($_COOKIE['security']) {
 	case 'low':
 		$vulnerabilityFile = 'low.php';
 		break;
@@ -43,13 +43,11 @@ $templateVars = array_merge($templateVars, [
 	'tokenField' => tokenField(),
 	'vulnerabilityFile' => $vulnerabilityFile,
 	'html' => $html,
-	'captchaKeySet' => $_DVWA[ 'recaptcha_public_key' ] == "",
-	'configPath' => realpath( getcwd() . DIRECTORY_SEPARATOR . DVWA_WEB_PAGE_TO_ROOT . "config" . DIRECTORY_SEPARATOR . "config.inc.php" ),
+	'captchaKeySet' => $_DVWA['recaptcha_public_key'] == "",
+	'configPath' => realpath(getcwd() . DIRECTORY_SEPARATOR . DVWA_WEB_PAGE_TO_ROOT . "config" . DIRECTORY_SEPARATOR . "config.inc.php"),
 	'hideForm' => $hide_form,
-	'recaptchaHtml' => recaptcha_get_html( $_DVWA[ 'recaptcha_public_key' ] ),
-
+	'recaptchaHtml' => recaptcha_get_html($_DVWA['recaptcha_public_key']),
 ]);
 
 echo $templates->render('vulnerabilities/captcha/index', $templateVars);
-
 ?>
