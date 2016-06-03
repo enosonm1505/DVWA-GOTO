92c8139022f77df2d73e66f3287ad85843754c26
Luud Janssen <luud@bitsensor.io>
2016-06-03 17:11:55 +0200

    Add captcha template

--- skins/default/templates/vulnerabilities/captcha/index.php
+++ skins/default/templates/vulnerabilities/captcha/index.php
@@ -5,50 +5,43 @@
 
 	<?php if (!$this->data['captchaKeySet']): ?>
         <div class="warning"><em>reCAPTCHA API key missing</em> from config file: <?= $this->data['configPath'] ?></div>
-    <? endif ?>
+    <?php endif ?>
 
 	<div class="vulnerable_code_area">
-		<form action="#" method="POST" ";
-
-if( $hide_form )
-    $page[ 'body' ] .= "style="display:none;"";
-
-$page[ 'body' ] .= ">
+		<form action="#" method="POST"<?= ($this->data['hideForm'] ? ' style="display: none;"' : '') ?>>
 			<h3>Change your password:</h3>
 			<br />
 
-			<input type="hidden" name="step" value="1" />\n";
+			<input type="hidden" name="step" value="1" />
+            <?php if ($this->data['vulnerabilityFile'] == 'impossible.php'): ?>
+            Current password:<br />
+            <input type="password" AUTOCOMPLETE="off" name="password_current"><br />
+            <?php endif ?>
+            New password:<br />
+            <input type="password" AUTOCOMPLETE="off" name="password_new"><br />
+            Confirm new password:<br />
+            <input type="password" AUTOCOMPLETE="off" name="password_conf"><br />
+            
+            <?= $this->data['recaptchaHtml'] ?>
 
-if( $vulnerabilityFile == 'impossible.php' ) {
-    $page[ 'body' ] .= "
-			Current password:<br />
-			<input type="password" AUTOCOMPLETE="off" name="password_current"><br />";
-}
+            <?php if ($this->data['vulnerabilityFile'] == 'high.php'): ?>
+            <!-- **DEV NOTE**   Response: 'hidd3n_valu3'   &&   User-Agent: 'reCAPTCHA'   **/DEV NOTE** -->
+            <?php endif ?>
 
-$page[ 'body' ] .= "			New password:<br />
-			<input type="password" AUTOCOMPLETE="off" name="password_new"><br />
-			Confirm new password:<br />
-			<input type="password" AUTOCOMPLETE="off" name="password_conf"><br />
-
-			" . recaptcha_get_html( $_DVWA[ 'recaptcha_public_key' ] );
-if( $vulnerabilityFile == 'high.php' )
-    $page[ 'body' ] .= "\n\n			<!-- **DEV NOTE**   Response: 'hidd3n_valu3'   &&   User-Agent: 'reCAPTCHA'   **/DEV NOTE** -->\n";
-
-if( $vulnerabilityFile == 'high.php' || $vulnerabilityFile == 'impossible.php' )
-    $page[ 'body' ] .= "\n			" . tokenField();
-
-$page[ 'body' ] .= "
+            <?php if ($this->data['vulnerabilityFile'] == 'high.php' || $this->data['vulnerabilityFile'] == 'impossible.php'): ?>
+            <?= $this->data['tokenField'] ?>
+            <?php endif ?>
 			<br />
 
 			<input type="submit" value="Change" name="Change">
 		</form>
-		{$html}
+		<?= $this->data['html'] ?>
 	</div>
 
 	<h2>More Information</h2>
 	<ul>
-		<li>" . dvwaExternalLinkUrlGet( 'http://www.captcha.net/' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.google.com/recaptcha/' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.owasp.org/index.php/Testing_for_Captcha_(OWASP-AT-012)' ) . "</li>
+		<li><?= $this->externalLink('http://www.captcha.net/') ?></li>
+		<li><?= $this->externalLink('https://www.google.com/recaptcha/') ?></li>
+		<li><?= $this->externalLink('https://www.owasp.org/index.php/Testing_for_Captcha_(OWASP-AT-012)') ?></li>
 	</ul>
 </div>
