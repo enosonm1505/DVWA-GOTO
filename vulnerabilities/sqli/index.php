8918aeade31b7da0ec3204a72dccba96f6b77a85
Luud Janssen <luud@bitsensor.io>
2016-06-03 19:52:23 +0200

    Add sqli templates

--- vulnerabilities/sqli/index.php
+++ vulnerabilities/sqli/index.php
@@ -1,5 +1,8 @@
 <?php
 
+$dir = dirname(__FILE__);
+$templates = require $dir.'/../../templates/templates.php';
+
 define( 'DVWA_WEB_PAGE_TO_ROOT', '../../' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
@@ -33,67 +36,31 @@ switch( $_COOKIE[ 'security' ] ) {
 
 require_once DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/sqli/source/{$vulnerabilityFile}";
 
-// Is PHP function magic_quotee enabled?
-$WarningHtml = '';
-if( ini_get( 'magic_quotes_gpc' ) == true ) {
-	$WarningHtml .= "<div class=\"warning\">The PHP function \"<em>Magic Quotes</em>\" is enabled.</div>";
-}
-// Is PHP function safe_mode enabled?
-if( ini_get( 'safe_mode' ) == true ) {
-	$WarningHtml .= "<div class=\"warning\">The PHP function \"<em>Safe mode</em>\" is enabled.</div>";
-}
-
-$page[ 'body' ] .= "
-<div class=\"body_padded\">
-	<h1>Vulnerability: SQL Injection</h1>
-
-	{$WarningHtml}
-
-	<div class=\"vulnerable_code_area\">";
-if( $vulnerabilityFile == 'high.php' ) {
-	$page[ 'body' ] .= "Click <a href=\"#\" onClick=\"javascript:popUp('session-input.php');return false;\">here to change your ID</a>.";
-}
-else {
-	$page[ 'body' ] .= "
-		<form action=\"#\" method=\"{$method}\">
-			<p>
-				User ID:";
-	if( $vulnerabilityFile == 'medium.php' ) {
-		$page[ 'body' ] .= "\n				<select name=\"id\">";
-		$query  = "SELECT COUNT(*) FROM users;";
-		$result = mysql_query( $query ) or die( '<pre>' . mysql_error() . '</pre>' );
-		$num    = mysql_result( $result, 0 );
-		$i      = 0;
-		while( $i < $num ) { $i++; $page[ 'body' ] .= "<option value=\"{$i}\">{$i}</option>"; }
-		$page[ 'body' ] .= "</select>";
+if ($vulnerabilityFile == 'medium.php') {
+	$query  = "SELECT COUNT(*) FROM users;";
+	$result = mysql_query($query) or die('<pre>' . mysql_error() . '</pre>');
+	$num    = mysql_result($result, 0);
+	$i      = 0;
+	$options = array();
+	while( $i < $num ) {
+		$i++;
+		array_push($options, $i);
 	}
-	else
-		$page[ 'body' ] .= "\n				<input type=\"text\" size=\"15\" name=\"id\">";
-
-	$page[ 'body' ] .= "\n				<input type=\"submit\" name=\"Submit\" value=\"Submit\">
-			</p>\n";
-
-	if( $vulnerabilityFile == 'impossible.php' )
-		$page[ 'body' ] .= "			" . tokenField();
-
-	$page[ 'body' ] .= "
-		</form>";
 }
-$page[ 'body' ] .= "
-		{$html}
-	</div>
 
-	<h2>More Information</h2>
-	<ul>
-		<li>" . dvwaExternalLinkUrlGet( 'http://www.securiteam.com/securityreviews/5DP0N1P76E.html' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://en.wikipedia.org/wiki/SQL_injection' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'http://ferruh.mavituna.com/sql-injection-cheatsheet-oku/' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'http://pentestmonkey.net/cheat-sheet/sql-injection/mysql-sql-injection-cheat-sheet' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'https://www.owasp.org/index.php/SQL_Injection' ) . "</li>
-		<li>" . dvwaExternalLinkUrlGet( 'http://bobby-tables.com/' ) . "</li>
-	</ul>
-</div>\n";
+$templateVars = getPageVariables($page);
+$templateVars = array_merge($templateVars, $templates->getTemplateVariables(DVWA_WEB_PAGE_TO_ROOT));
+$templateVars = array_merge($templateVars, [
+	'title' => $page['title'],
+	'tokenField' => tokenField(),
+	'vulnerabilityFile' => $vulnerabilityFile,
+	'html' => $html,
+	'magicQuotesEnabled' => ini_get('magic_quotes_gpc'),
+	'safeModeEnabled' => ini_get('safe_mode'),
+	'method' => $method,
+	'options' => $options
+]);
 
-dvwaHtmlEcho( $page );
+echo $templates->render('vulnerabilities/sqli/index', $templateVars);
 
 ?>
