e4d5ffbf2b2fbb7915363bed72c7526dbac3c003
enosonm1505 <91190634+enosonm1505@users.noreply.github.com>
2022-06-22 21:22:46 +0530

    Update rulechef.rb

--- rulechef.rb
+++ rulechef.rb
@@ -4,10 +4,6 @@ describe package('telnetd') do
   it { should_not be_installed }
 end
 
-describe package('http') do
-  it { should_not be_installed }
-end
-
 describe inetd_conf do
   its("telnet") { should eq nil }
 end
